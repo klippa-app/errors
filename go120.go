86668b0b181f15ce29bca69972126e1ffd1beacc
Jeroen Bobbeldijk <jeroen@klippa.com>
2024-07-12 14:08:14 +0200

    Add Join support

---
+++ go120.go
@@ -0,0 +1,27 @@
+//go:build go1.20
+// +build go1.20
+
+package errors
+
+import (
+	stderrors "errors"
+)
+
+// Join returns an error that wraps the given errors.
+// Any nil error values are discarded.
+// Join returns nil if every value in errs is nil.
+// The error formats as the concatenation of the strings obtained
+// by calling the Error method of each element of errs, with a newline
+// between each string.
+//
+// A non-nil error returned by Join implements the Unwrap() []error method.
+func Join(errs ...error) error {
+	joined := stderrors.Join(errs...)
+	if joined == nil {
+		return nil
+	}
+	return &withStack{
+		joined,
+		callers(),
+	}
+}
